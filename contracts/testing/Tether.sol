d2815c65e129185eb836974b8089b3f6f27d1fee
andrew-sol <andrew.sologor@gmail.com>
2024-02-16 10:22:11 +0000

    docs: add docs, fix .env.example

--- contracts/testing/Tether.sol
+++ contracts/testing/Tether.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.23;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 /**
  * This is a fake Tether (USDT) contract made for testing purposes.
